18381a62a9927ad84bd2bd6ff8150ec4506dcfef
Nikolai Tandberg <nikoltan@stud.ntnu.no>
2023-11-13 12:40:49 +0100

    feat(TrainDispatchApp): Add main, init and start methods

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -1,8 +1,45 @@
 package edu.ntnu.stud;
 
 /**
- * This is the main class for the train dispatch application.
+ * This is the application class for the train dispatch system, containing the main method.
+ *
+ * @author nikolaitandberg
+ * @version 1.0
+ * @since 2023-13-11
  */
 public class TrainDispatchApp {
-  // TODO: Fill in the main method and any other methods you need.
+  private static DepartureRegistry departureRegistry;
+
+  /**
+   * initializes registry needed to run application and adds some starting data.
+   */
+  private static void init() {
+    departureRegistry = new DepartureRegistry();
+
+    departureRegistry.addDeparture("14:00", "L2", 55, "Drammen", 2, "00:10");
+    departureRegistry.addDeparture("09:00", "R13", 19, "Lillestrøm", 3, "00:00");
+    departureRegistry.addDeparture("08:45", "R11", 77, "Arendal", 1, "00:20");
+
+  }
+
+  /**
+   * Runs the user interface.
+   */
+  private static void start() {
+
+    // TODO: switchcase and while-loop
+
+
+    System.out.println(departureRegistry.getDeparturesByDestination("Drammen"));
+  }
+
+  public static void main(String[] args) {
+    init();
+    start();
+  }
+
 }
+
+
+
+
