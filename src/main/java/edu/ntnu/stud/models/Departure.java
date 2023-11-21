3800af1c760e43f6fd877d756722ba8acaeba85a
Nikolai Tandberg <nikoltan@stud.ntnu.no>
2023-11-21 12:28:43 +0100

    docs(Departure): Clean up javadoc for Departure and group exception throws

--- src/main/java/edu/ntnu/stud/models/Departure.java
+++ src/main/java/edu/ntnu/stud/models/Departure.java
@@ -1,6 +1,6 @@
 package edu.ntnu.stud.models;
 
-import edu.ntnu.stud.Utils;
+import edu.ntnu.stud.utils.TimeHandling;
 import java.time.LocalTime;
 
 /**
@@ -18,17 +18,22 @@ public class Departure {
   private final String destination;
   private int track;
   private LocalTime delay;
-  /**
-     * Constructor.
-     *
-     * @param departureTime the time of day the train departs
-     * @param line the line the train is travelling on
-     * @param trainNumber the unique number of the departure
-     * @param destination the last stop on the line
-     * @param track the track at which the train arrives at the station
-     * @param delay the amount of time the train is delayed compared to it's scheduled time
-     */
 
+  /**
+   * Constructs an object of the Departure class.
+   * Takes the following parameters
+   * May throw IllegalArgumentException
+   *
+   * @param departureTime the time of day the train departs
+   * @param line the train is travelling on
+   * @param trainNumber the unique number of the departure
+   * @param destination the last stop on the line
+   * @param track the track at which the train arrives at the station
+   * @throws IllegalArgumentException When line is empty,
+   *                                  when train number is less than 1,
+   *                                  when destination is empty
+   *                                  when track is 0 or a negative number other than -1
+   */
   public Departure(
           String departureTime,
           String line,
@@ -36,44 +41,46 @@ public class Departure {
           String destination,
           int track,
           String delay
-  ) {
-    if (departureTime.isEmpty()) {
-      throw new IllegalArgumentException("Departure time cannot be left empty");
-    }
-    this.departureTime = Utils.parseTimeString(departureTime);
+  ) throws IllegalArgumentException {
     if (line.isEmpty()) {
       throw new IllegalArgumentException("Line cannot be left empty");
     }
-    this.line = line;
     if (trainNumber < 1) {
       throw new IllegalArgumentException("Train number cannot be less than 1");
     }
-    this.trainNumber = trainNumber;
     if (destination.isEmpty()) {
       throw new IllegalArgumentException("Destination cannot be left empty");
     }
+
+    this.departureTime = TimeHandling.parseTimeString(departureTime);
+    this.line = line;
+    this.trainNumber = trainNumber;
     this.destination = destination;
+    this.setTrack(track);
+    this.delay = TimeHandling.parseTimeString(delay);
+  }
+
+  // Setters
+
+  /**
+   * Sets a new track for the departure and checks if the track is and illegal value.
+   * @param track the new track for the train to arrive at the station
+   * @throws IllegalArgumentException when track is 0 or less than -1
+   */
+  public void setTrack(int track) throws IllegalArgumentException {
     if (track < -1 || track == 0) {
       throw new IllegalArgumentException("Track cannot be 0 or a negative number other than -1");
     }
     this.track = track;
-    if (delay.isEmpty()) {
-      throw new IllegalArgumentException("Delay cannot be left empty");
-    }
-    this.delay = Utils.parseTimeString(delay);
   }
 
-  // Setters
-  public void setTrack(int track) {
-    this.track = track;
-  }
-
-  /** set new delay.
- *
- * @param delay a string on format "HH:mm"
- */
+  /**
+   * set new delay and uses TimeHandling class to parse from string parameter to LocalTime object.
+   *
+   * @param delay a string on format "HH:mm"
+   */
   public void setDelay(String delay) {
-    this.delay = Utils.parseTimeString(delay);
+    this.delay = TimeHandling.parseTimeString(delay);
   }
 
   // Getters
